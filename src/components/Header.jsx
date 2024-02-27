2dfd9e75167c07bc4775dd7717edf92bb1a21d67
Sana Yoosuf <sanazaier777@gmail.com>
2024-02-27 16:14:43 +0400

    react revision

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+<div className="header">
+    <div className="logo">AL Zaier</div>
+    <div >
+        <ul className="menu">
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</div>
+  )
+  
+};
+export default Header;
---
+++ src/components/Product.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+const Product = (props) => {
+  const [available,setAvailable]=useState("Now available")
+  function btnClick(data){
+    console.log("Item added to cart" + data)
+    
+  }
+  function setAvailableProduct()
+  {
+    setAvailable("No Stock")
+    console.log("Sorry..Product out of stock")
+  }
+    
+  
+  return (
+    
+    
+     <div className="product">
+      
+      <img src={props.url} alt="product-image"/>
+      <h3>{props.name}</h3>
+      <p>{props.price}</p>
+
+      <button className="btn" onClick={(event)=>{
+        console.log(event)
+        btnClick("confirm")
+      }}>Clickme</button>
+      <button className="btn" onClick={setAvailableProduct}>Buy Me</button>
+      <p>{available}</p>
+    </div>
+   
+   
+  );
+};
+export default Product;
